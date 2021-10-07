784ede183843e94146cf61f9d287e165990669af
Zidan Ramadhan <zidan.ramadhan@ui.ac.id>
2021-10-07 16:13:23 +0700

    TP Migrasi ke Eclipse IDE Zidan Ramadhan

--- zidanJmartKD/Jmart.java
+++ zidanJmartKD/Jmart.java
@@ -4,10 +4,6 @@ public class Jmart
 {
     public static void main(String[] args)
     {
-        Account betaTest = new Account(456, "Gae Hwi - 456", "gaehwi@gmail.com", "Gaehwi456");
-        System.out.println(betaTest.validate());
-        
-        Complaint betaComplaint = new Complaint(99, "Terlambat datang");
-        System.out.println(betaComplaint);
+        System.out.print("Hello from Eclipse!");
     }
 }
